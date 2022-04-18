9eb529a58352bfe85119caf43a2e13b859f4b99b
赵光帅 <969426998@qq.com>
2022-04-18 19:16:25 +0800

    用户模型

---
+++ app/models/user/user_model.go
@@ -0,0 +1,15 @@
+// Package user 存放用户 Model 相关逻辑
+package user
+
+import "gohub/app/models"
+
+type User struct {
+	models.BaseModel
+
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"-"`
+	Phone    string `json:"-"`
+	Password string `json:"-"`
+
+	models.CommonTimestampsField
+}
