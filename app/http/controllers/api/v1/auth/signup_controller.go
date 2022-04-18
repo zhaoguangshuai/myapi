116046a739ecfac3473d5fa1807f0ad5cf12703c
赵光帅 <969426998@qq.com>
2022-04-18 19:39:04 +0800

    手机号是否注册接口

---
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -0,0 +1,39 @@
+// Package auth 处理用户身份认证相关逻辑
+package auth
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/models/user"
+	"net/http"
+)
+
+type SignupController struct {
+	v1.BaseAPIController
+}
+
+func (sc *SignupController) IsPhoneExist(c *gin.Context) {
+
+	// 请求对象
+	type PhoneExistRequest struct {
+		Phone string `json:"phone"`
+	}
+	request := PhoneExistRequest{}
+	// 解析 json 请求
+	if err := c.ShouldBindJSON(&request); err != nil {
+		// 解析失败
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err.Error(),
+		})
+		// 打印错误信息
+		fmt.Println(err.Error())
+		// 出错了，中断请求
+		return
+	}
+
+	// 检查数据库并返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exist": user.IsPhoneExist(request.Phone),
+	})
+}
--- routes/api.go
+++ routes/api.go
@@ -2,7 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"gohub/app/http/controllers/api/v1/auth"
 )
 
 // RegisterAPIRoutes 注册网页相关路由
@@ -10,12 +10,11 @@ func RegisterAPIRoutes(r *gin.Engine) {
 	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
 	{
-		// 注册一个路由
-		v1.GET("/", func(c *gin.Context) {
-			// 以 JSON 格式响应
-			c.JSON(http.StatusOK, gin.H{
-				"Hello": "World!",
-			})
-		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// Package auth 处理用户身份认证相关逻辑
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
 }
