135063e1e0446f3eeb9617fd89e8eb5b11be0557
赵光帅 <969426998@qq.com>
2022-04-19 11:06:03 +0800

    panic recovery

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -32,7 +32,8 @@ func registerGlobalMiddleWare(router *gin.Engine) {
 	router.Use(
 		// gin.Logger(),
 		middlewares.Logger(),
-		gin.Recovery(),
+		// gin.Recovery(),
+		middlewares.Recovery(),
 	)
 }
 
