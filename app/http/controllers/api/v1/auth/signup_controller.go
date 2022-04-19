03bf82f01460f231e4f6d675e42ccdaf0df303f0
赵光帅 <969426998@qq.com>
2022-04-19 15:50:21 +0800

    response 包

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -6,7 +6,7 @@ import (
 	v1 "gohub/app/http/controllers/api/v1"
 	"gohub/app/models/user"
 	"gohub/app/requests"
-	"net/http"
+	"gohub/pkg/response"
 )
 
 type SignupController struct {
@@ -20,9 +20,12 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 		return
 	}
 	// 检查数据库并返回响应
-	c.JSON(http.StatusOK, gin.H{
+	response.JSON(c, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
 	})
+	//c.JSON(http.StatusOK, gin.H{
+	//	"exist": user.IsPhoneExist(request.Phone),
+	//})
 }
 
 func (sc *SignupController) IsEmailExist(c *gin.Context) {
@@ -34,7 +37,10 @@ func (sc *SignupController) IsEmailExist(c *gin.Context) {
 	}
 
 	// 检查数据库并返回响应
-	c.JSON(http.StatusOK, gin.H{
+	response.JSON(c, gin.H{
 		"exits": user.IsEmailExist(request.Email),
 	})
+	//c.JSON(http.StatusOK, gin.H{
+	//	"exits": user.IsEmailExist(request.Email),
+	//})
 }
--- app/requests/requests.go
+++ app/requests/requests.go
@@ -2,7 +2,7 @@
 package requests
 
 import (
-	"fmt"
+	"gohub/pkg/response"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -20,11 +20,11 @@ func Validate(c *gin.Context, obj interface{}, handler ValidatorFunc) bool {
 
 	// 1. 解析请求，支持 JSON 数据、表单请求和 URL Query
 	if err := c.ShouldBind(obj); err != nil {
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"message": "请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。",
-			"error":   err.Error(),
-		})
-		fmt.Println(err.Error())
+		response.BadRequest(c, err, "请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。")
+		//c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+		//	"message": "请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。",
+		//	"error":   err.Error(),
+		//})
 		return false
 	}
 
